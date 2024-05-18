cece0ad22bc693705b2d4decf4a5cc29e87eb50c
Gregory Salnikov <greengo86@mail.ru>
2024-05-18 13:21:08 +0400

    FIX ER;

--- str_func.py
+++ str_func.py
@@ -1,5 +1,19 @@
+# внесены изменения!
 def get_upper_case(word: str):
+    """
+    accepts a string, returns a string with all capital letters
+    :param word: str
+    :return: str
+    """
     return word.upper()
 
-def get_upper_(word: str):
-    return word.upper()
+
+def get_title(word: str):
+    """
+    Accepts a string and return a string
+    in which all the first letters of the words are uppercase
+    :param word: str
+    :return: str
+    """
+    return word.title()
+
